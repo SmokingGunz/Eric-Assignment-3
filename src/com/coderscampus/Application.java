25786894a6d3431298761334c23abe427ddaf008
SmokingGunz <ericowens08@gmail.com>
2022-12-23 17:20:17 -0800

    v4

--- src/com/coderscampus/Application.java
+++ src/com/coderscampus/Application.java
@@ -1,39 +1,18 @@
 package com.coderscampus;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Application {
 
-	public static User[] users = new User[4];
+//	public static User[] users = new User[4];
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		// Make it a method that returns a user array!
-		try (BufferedReader fileReader = new BufferedReader(new FileReader("data.txt"))) {
-			String line;
-			while ((line = fileReader.readLine()) != null) {
-				UserService userService = new UserService();
-				String userInfo = line;
-				String[] userData = userService.parseText(userInfo);
-				User user = userService.createUser(userData);
-				System.out.println(user);
-			
-
-//				for (int i = 0; i < 4; i++) {
-//					users[i] = userService.createUser(userData);
-//				}
-// then for each line made into an array of user information create a user. 
-			}
-		} catch (FileNotFoundException e) {
-			throw e;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		UserService app = new UserService();
+		User[] users = app.getUserArray();
+		for (User user : users) {
+			System.out.println(user);
 		}
-
 	}
 
 }
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -1,16 +1,22 @@
 package com.coderscampus;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class UserService {
 
 	public User validUser(String username, String password) {
+		return null;
 		
-		for(User user : Application.users) {
-			
-			
-			
+//		for(User user : Application.users) {
+//			
+//			
+//			
 		}
 
-	public User createUser(String[] stringInputs) {
+	public static User createUser(String[] stringInputs) {
 
 		String userName = stringInputs[0];
 		String passWord = stringInputs[1];
@@ -23,11 +29,37 @@ public class UserService {
 
 	}
 
-	public String[] parseText(String inputs) {
+	public static String[] parseText(String inputs) {
 
 		String input = inputs;
 		String[] textParse = input.split(",");
 		return textParse;
 
+	}
+
+	public User[] getUserArray() throws FileNotFoundException {
+		User[] users = new User[4];
+//		UserService userService = new UserService();
+		String[] userData = null;
+		int i = 0;
+		try (BufferedReader fileReader = new BufferedReader(new FileReader("data.txt"))) {
+			String line;
+			while ((line = fileReader.readLine()) != null) {
+				String userInfo = line;
+				userData = parseText(userInfo);
+				users[i] = createUser(userData);
+				i++;
+
+			}
+		} catch (FileNotFoundException e) {
+			throw e;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		for(User user : users) {
+//			System.out.println(user);
+//		}
+		return users;
 	}
 }
